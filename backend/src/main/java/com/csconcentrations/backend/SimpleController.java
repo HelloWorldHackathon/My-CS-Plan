121eb0d2776c6a3ccd5c18f570d193360aa2b876
Jaimss <james@harrell365.com>
2021-09-18 10:59:55 -0400

    import

--- backend/src/main/java/com/csconcentrations/backend/SimpleController.java
+++ backend/src/main/java/com/csconcentrations/backend/SimpleController.java
@@ -1,3 +1,10 @@
+package com.csconcentrations.backend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class SimpleController {
     @Value("${spring.application.name}")
