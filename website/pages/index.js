99434dd6c80e61fc444023bbfeddcb4c87ef27b1
Jonathan Oppenheimer <147infiniti@gmail.com>
2021-09-18 15:10:43 -0400

    Fix submission method and add sample output

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -15,80 +14,87 @@ export default function Home() {
       <h1 class="title">Welcome to My CS Plans</h1>
 
       <h1>Concentrations:</h1>
-      <div class="threebythree">
-        <div class="item">
-          <label class="checkboxList">
-            Computational Science and Engineering
-            <input type="checkbox" onClick={() => getTrack("Computational Science and Engineering")} />
-            <span class="checkmark"></span>
-          </label>
+      <form>
+        <div class="threebythree">
+          <div class="item">
+            <label class="checkboxList">
+              Computational Science and Engineering
+              <input
+                type="checkbox"
+                value="Computational Science and Engineering"
+              />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              Computer Graphics and Visualization
+              <input
+                type="checkbox"
+                value="Computer Graphics and Visualization"
+              />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              Database and Information Systems
+              <input type="checkbox" value="Database and Information Systems" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              (Algorithmic) Foundations
+              <input type="checkbox" value="(Algorithmic) Foundations" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              Machine Intelligence
+              <input type="checkbox" value="Machine Intelligence" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              Programming Language
+              <input type="checkbox" value="Programming Language" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              Security
+              <input type="checkbox" value="Security" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              Software Engineering
+              <input type="checkbox" value="Software Engineering" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
+          <div class="item">
+            <label class="checkboxList">
+              Systems Software
+              <input type="checkbox" value="Systems Software" />
+              <span class="checkmark"></span>
+            </label>
+          </div>
         </div>
-        <div class="item">
-          <label class="checkboxList">
-            Computer Graphics and Visualization
-            <input type="checkbox" onClick={() => getTrack("Computer Graphics and Visualization")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        <div class="item">
-          <label class="checkboxList">
-            Database and Information Systems Track
-            <input type="checkbox" onClick={() => getTrack("Database and Information Systems Track")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
+        <button class="submit-button" type="button" onClick={() => getTrack(" ")}>Submit</button>
 
-        <div class="item">
-          <label class="checkboxList">
-            (Algorithmic) Foundations Track
-            <input type="checkbox" onClick={() => getTrack("(Algorithmic) Foundations Track")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        <div class="item">
-          <label class="checkboxList">
-            Machine Intelligence Track
-            <input type="checkbox" onClick={() => getTrack("Machine Intelligence Track")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        <div class="item">
-          <label class="checkboxList">
-            Programming Language Track
-            <input type="checkbox" onClick={() => getTrack("Programming Language Track")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-
-        <div class="item">
-          <label class="checkboxList">
-            Security Track
-            <input type="checkbox" onClick={() => getTrack("Security Track")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        <div class="item">
-          <label class="checkboxList">
-            Software Engineering
-            <input type="checkbox" onClick={() => getTrack("Software Engineering")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-        <div class="item">
-          <label class="checkboxList">
-            Systems Software Track
-            <input type="checkbox" onClick={() => getTrack("Systems Software Track")} />
-            <span class="checkmark"></span>
-          </label>
-        </div>
-      </div>
+      </form>
 
       <hr />
 
-      <div class="dynamicContent">
-        <p id="text" style={{ display: "none" }}>
-          Checkbox is CHECKED!
-        </p>
+      <div id="dynamicText">
+        <h2 id="titleArea">Select some tracks!</h2>
+        <div id="tableArea"></div>
       </div>
 
       <footer class="footer">
@@ -106,6 +112,24 @@ export default function Home() {
 
 function getTrack(track) {
   var checkBox = document.getElementById("myCheck");
-  var text = document.getElementById("text");
-  console.log(track);
+
+  // Gets names of tracks student selected and create a string of them, before setting title to the names of selected tracks
+  let trackList = "Tracks selected: ";
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  for (var checkbox of checkboxes) {
+    trackList = trackList + checkbox.value + ", ";
+  }
+  trackList = trackList.slice(0, -2); //remove last comma
+  console.log(trackList);
+  document.getElementById("titleArea").innerHTML = trackList;
+
+  // Generates table from JSON optimized classes list and inserts it below 
+
+  let table = " "
+  /* INSERT LOOP CODE FOR JSON HERE*/
+
+  //Sample table for visual purposes
+  table = table + "<table>  <tr>    <th>Company</th>    <th>Contact</th>    <th>Country</th>  </tr>  <tr>    <td>Alfreds Futterkiste</td>    <td>Maria Anders</td>    <td>Germany</td>  </tr>  <tr>    <td>Centro comercial Moctezuma</td>    <td>Francisco Chang</td>    <td>Mexico</td>  </tr>  <tr>    <td>Ernst Handel</td>    <td>Roland Mendel</td>    <td>Austria</td>  </tr>  <tr>    <td>Island Trading</td>    <td>Helen Bennett</td>    <td>UK</td>  </tr>  <tr>    <td>Laughing Bacchus Winecellars</td>    <td>Yoshi Tannamuri</td>    <td>Canada</td>  </tr>  <tr>    <td>Magazzini Alimentari Riuniti</td>    <td>Giovanni Rovelli</td>    <td>Italy</td>  </tr></table>"
+
+  document.getElementById("tableArea").innerHTML = table;
 }
