a596dc592d11b54dcb352fb7f9d8acd79a24afb9
Pranav Kammili <thekammilian@pal-nat186-15-216.itap.purdue.edu>
2021-09-18 13:46:51 -0400

    Card.js added again(???)

---
+++ website/components/Card.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <html>
+        <h1>CS Class Titles </h1>
+        <div className = "cardBox">
+            <div>
+                
+            </div>
+        </div>
+    </html>
+  )
+}
