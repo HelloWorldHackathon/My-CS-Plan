8689f506ade81ba995b9fe60ede6a56761ed103f
Jonathan Oppenheimer <147infiniti@gmail.com>
2021-09-18 10:49:47 -0400

    Backend setup

---
+++ backend/src/main/java/com/csconcentrations/backend/SimpleController.java
@@ -0,0 +1,11 @@
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
