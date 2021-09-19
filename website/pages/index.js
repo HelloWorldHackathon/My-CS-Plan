27be586a74acc9d88bec6f9d27a185f3bdac7708
Jaimss <james@harrell365.com>
2021-09-19 12:35:53 -0400

    url

--- website/pages/index.js
+++ website/pages/index.js
@@ -231,7 +231,7 @@ function getTrack() {
   if (process.env.NODE_ENV == "development") {
     url = "http://127.0.0.1:8081"
   } else {
-    url = "backend:8081"
+    url = "http://backend:8081"
   }
   console.log(url)
 
