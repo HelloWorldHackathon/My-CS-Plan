fc25ce7a90fd1a9f16823652118b0931c7e1c5b8
Jaimss <james@harrell365.com>
2021-09-18 22:19:49 -0400

    t

--- backend/flaskr/app.py
+++ backend/flaskr/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
 from util import compare_tracks, format_schedule, get_hours, load_csv
+import json
 
 app = Flask(__name__)
 data = []
 
-@app.route("/<classone>/<classtwo>")
+
 def classes(classone, classtwo):
     dd = {}
 
@@ -19,14 +20,15 @@ def classes(classone, classtwo):
     compared.remove(electives2)
 
     courses = compared
-    courses = list(map(lambda x: [" OR ".join(x) if type(x) != str else x, format_schedule(x), str(get_hours(x))], courses))
+    courses = list(map(lambda x: [" OR ".join(x) if type(
+        x) != str else x, format_schedule(x), str(get_hours(x))], courses))
     dd["courses"] = courses
 
     required = electives1[0]
     electives1.pop(0)
     electives_data = {
         "required": required,
-        "courses": list(map(lambda x: [" OR ".join(x) if type(x) != str else x, format_schedule(x), str(get_hours(x))], electives1)) 
+        "courses": list(map(lambda x: [" OR ".join(x) if type(x) != str else x, format_schedule(x), str(get_hours(x))], electives1))
     }
     dd["electives1"] = electives_data
 
@@ -34,7 +36,7 @@ def classes(classone, classtwo):
     electives2.pop(0)
     electives_data = {
         "required": required,
-        "courses": list(map(lambda x: [" OR ".join(x) if type(x) != str else x, format_schedule(x), str(get_hours(x))], electives2)) 
+        "courses": list(map(lambda x: [" OR ".join(x) if type(x) != str else x, format_schedule(x), str(get_hours(x))], electives2))
     }
     dd["electives2"] = electives_data
 
@@ -44,4 +46,10 @@ def classes(classone, classtwo):
 if __name__ == "__main__":
     data = load_csv()
 
-    app.run(host="0.0.0.0", port=8081)
+    # app.run(host="0.0.0.0", port=8081)
+
+    for i in range(0,9 ):
+        for j in range(0, 9):
+            with open(f"nums/{i}_{j}.json", 'w') as f:
+                dd = classes(i, j)
+                f.write(json.dumps(dd))
